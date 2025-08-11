5237e7ae388d2f32db281a568ba10e6f75a277a5
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2025-08-11 16:15:40 -0500

    other test

--- app/lib/sanity.jsx
+++ app/lib/sanity.jsx
@@ -3,8 +3,8 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   apiVersion: "2025-07-30",
-  dataset: process.env.SANITY_DATASET,
-  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: "production",
+  projectId: "in8j457v",
   useCdn: false,
   token: process.env.SANITY_TOKEN,
 });
