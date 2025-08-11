baddec26fb5862c9ec2b3bc42cafad3f4874dc4d
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2025-08-11 16:09:50 -0500

    testing enviroment problems

--- app/lib/sanity.jsx
+++ app/lib/sanity.jsx
@@ -3,8 +3,8 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   apiVersion: "2025-07-30",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  projectId: process.env.SANITY_PROJECT_ID,
   useCdn: false,
   token: process.env.SANITY_TOKEN,
 });
