ef9374e2bf7d76748ea61df31133f71fbf51744c
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2025-08-14 12:05:26 -0500

    fix home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -13,12 +13,16 @@ export default function Home() {
   const data: Blog[] = blogs;
   return (
     <div>
+      {/* <div className="bg-tertiary-foreground w-full h-screen rounded-lg mb-6 lg:-mt-10 "></div> */}
       <News title={"Ultimas noticias"} data={data} />
+      <div className="w-full shadow-2xl rounded-xl p-6 bg-tertiary-foreground my-6">
+        <Simulators />
+      </div>
+
       <div className="flex flex-col lg:flex-row py-10 gap-8">
         <ArticleHome />
         <RecommendedTags />
       </div>
-      <Simulators />
     </div>
   );
 }
--- components/News/News.tsx
+++ components/News/News.tsx
@@ -1,8 +1,6 @@
 import { Blog } from "@/lib/interface";
 import React from "react";
 import NewsCard from "../CommonComponents/Cards/NewsCard";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
 
 interface INewsProps {
   title?: string;
@@ -15,11 +13,11 @@ const News = ({ data, title }: INewsProps) => {
       <h2 className="text-xl lg:text-2xl bg-primary-foreground text-background px-6 lg:px-10 py-2 lg:py-2 rounded-full mx-auto font-agrandir font-bold text-shadow-md  text-shadow-black/20">
         {title}
       </h2>
-      <div className="flex flex-col lg:flex-row gap-10 w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:w-2/3">
+      <div className="flex flex-col gap-10 w-full mt-5">
+        <div className="hidden lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:w-full">
           {data.length > 0 &&
             data
-              .slice(0, 4)
+              .slice(0, 3)
               .map((post, idx) => (
                 <NewsCard
                   key={idx}
@@ -36,8 +34,71 @@ const News = ({ data, title }: INewsProps) => {
                 />
               ))}
         </div>
-        <div className="w-1/3 h-96 bg-tertiary">
-          <p className="text-5xl"> publicidad</p>
+        <div className="lg:hidden grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:w-full">
+          {data.length > 0 &&
+            data
+              .slice(0, 2)
+              .map((post, idx) => (
+                <NewsCard
+                  key={idx}
+                  excerpt={post.excerpt}
+                  image={post.mainImage}
+                  slug={post.slug}
+                  title={
+                    post.title +
+                    " " +
+                    (post?.focusTitle || "") +
+                    " " +
+                    (post?.continueTitle || "")
+                  }
+                />
+              ))}
+        </div>
+
+        <div className="w-full h-40 bg-tertiary">
+          <p className="text-5xl"> publicidad </p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-10 w-full mb-10">
+        <div className="hidden lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:w-full">
+          {data.length > 3 &&
+            data
+              .slice(3, 6)
+              .map((post, idx) => (
+                <NewsCard
+                  key={idx}
+                  excerpt={post.excerpt}
+                  image={post.mainImage}
+                  slug={post.slug}
+                  title={
+                    post.title +
+                    " " +
+                    (post?.focusTitle || "") +
+                    " " +
+                    (post?.continueTitle || "")
+                  }
+                />
+              ))}
+        </div>
+        <div className="lg:hidden grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:w-full">
+          {data.length > 2 &&
+            data
+              .slice(2, 4)
+              .map((post, idx) => (
+                <NewsCard
+                  key={idx}
+                  excerpt={post.excerpt}
+                  image={post.mainImage}
+                  slug={post.slug}
+                  title={
+                    post.title +
+                    " " +
+                    (post?.focusTitle || "") +
+                    " " +
+                    (post?.continueTitle || "")
+                  }
+                />
+              ))}
         </div>
       </div>
     </div>
