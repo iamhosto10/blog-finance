12c729d26398d28d00cd303fc1dff093eb126d30
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2025-10-06 10:28:04 -0500

    fix news card

--- components/CommonComponents/Cards/NewsCard.tsx
+++ components/CommonComponents/Cards/NewsCard.tsx
@@ -32,22 +32,24 @@ const NewsCard = (props: INewsCard) => {
   return (
     <Link
       href={`/${mainCategory}/${slug?.current}`}
-      className="w-full flex flex-col h-full justify-between gap-1 hover:scale-110 transition-all"
+      className="w-full flex flex-col h-full gap-2 hover:scale-110 transition-all"
     >
-      <h2 className="text-lg line-clamp-3 text-secondary font-agrandir font-bold">
+      <h2 className="text-lg line-clamp-4 text-secondary font-agrandir font-bold">
         {title}
         <span className="text-primary"> {focusTitle} </span>
         {continueTitle}
       </h2>
-      <img
-        src={urlFor(image).url()}
-        alt="image"
-        height={500}
-        className="rounded-lg object-cover w-full h-48 xl:h-64"
-      />
-      <p className="line-clamp-5 text-sm mt-2 text-tertiary font-canva-sans text-justify">
-        {excerpt}
-      </p>
+      <div className="flex flex-col mt-auto gap-2">
+        <img
+          src={urlFor(image).url()}
+          alt="image"
+          height={500}
+          className="rounded-lg object-cover w-full h-48 xl:h-64"
+        />
+        <p className="line-clamp-5 text-sm  text-tertiary font-canva-sans text-justify ">
+          {excerpt}
+        </p>
+      </div>
     </Link>
   );
 };
