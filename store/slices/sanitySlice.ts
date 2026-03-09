22ba16a0a1c7bdf099a8fbdf7da47e584917bd07
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2026-03-09 17:27:49 -0500

    fix layout home and delete redux

--- store/slices/sanitySlice.ts
+++ store/slices/sanitySlice.ts
@@ -1,47 +1,23 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {
-  Bank,
-  Blog,
-  Card,
-  Category,
-  Dolar,
-  Franchise,
-  Profitability,
-} from "@/lib/interface";
 
-export interface SanityState {
-  blogs: Blog[];
-  categories: Category[];
-  dolar: Dolar | null;
-  profitability: Profitability | null;
-  banks: Bank[] | null;
-  franchieses: Franchise[] | null;
-  cards: Card[] | null;
-  loading: boolean;
-  error: string | null;
+interface SanityState {
+  categories: any[];
+  isMenuOpen?: boolean;
 }
 
 const initialState: SanityState = {
-  blogs: [],
   categories: [],
-  dolar: null,
-  profitability: null,
-  banks: null,
-  franchieses: null,
-  cards: null,
-  loading: false,
-  error: null,
 };
 
-const sanitySlice = createSlice({
+export const sanitySlice = createSlice({
   name: "sanity",
   initialState,
   reducers: {
-    setPreloadedData: (state, action: PayloadAction<SanityState>) => {
-      return { ...state, ...action.payload, loading: false, error: null };
+    setCategories: (state, action: PayloadAction<any[]>) => {
+      state.categories = action.payload;
     },
   },
 });
 
-export const { setPreloadedData } = sanitySlice.actions;
+export const { setCategories } = sanitySlice.actions;
 export default sanitySlice.reducer;
